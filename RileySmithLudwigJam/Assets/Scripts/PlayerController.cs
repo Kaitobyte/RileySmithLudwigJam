543ad87ebb49c56b13e7db4c376a0548c2124be9
ryethelight <ryethelight1@gmail.com>
2021-10-19 19:48:19 -0400

    Made the rope stay connected after grappling
    Had some good success but also introduced a few new issues. Really need to get movement hammered out before I can keep... moving. (sigh)

--- RileySmithLudwigJam/Assets/Scripts/PlayerController.cs
+++ RileySmithLudwigJam/Assets/Scripts/PlayerController.cs
@@ -5,25 +5,68 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float accel;
+    [SerializeField] private float friction;
     [SerializeField] private float gravity;
+
+    private float ropeLength, xSpeed, ySpeed, xDirection, yDirection;
+    private Vector3 grappleLocation, playerVelocity;
+    private LineRenderer grapple;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        grapple = GetComponentInChildren<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        if (grapple.GetComponent<Rope>().grappleEnded())
+        {
+            ropeLength = grapple.GetComponent<Rope>().ropeLength();
+            grappleLocation = grapple.GetComponent<Rope>().grappleLocation();
+
+            if (ropeLength < Mathf.Abs(transform.position.x - grappleLocation.x))
+            {
+                playerVelocity.x = -playerVelocity.x;
+            }
+
+            if (ropeLength < Mathf.Abs(transform.position.y - grappleLocation.y))
+            {
+                playerVelocity.y = -playerVelocity.y;
+            }
+
+        }
+
+
+        //to-do change movement so that it goes based on player velocity
+        //test to see if player movement is restricted by the length of the rope
+        //add gravity to see if player hangs from rope, add/restrict movement in the air
+
+
         // -1 for left / down
         // +1 for right / up
-        float xDirection = Input.GetAxis("Horizontal");
-        float yDirection = Input.GetAxis("Vertical");
+        xDirection = Input.GetAxis("Horizontal");
+        yDirection = Input.GetAxis("Vertical");
 
-        Vector3 moveDirection = new Vector3(xDirection, 0.0f, 0.0f);
+        //to-do account for movement in both directions. This only applies friction when moving right/up
+        //also the player just falls/launches left right now so fix that
+        xSpeed += (xDirection * accel) - friction;
+        ySpeed += (yDirection * accel) - friction;
+        
+        //to-do add sneak, walk, run, and mid-grapple speeds
+        if (xSpeed > walkSpeed)
+        {
+            xSpeed = walkSpeed;
+        }
 
-        transform.position += moveDirection * moveSpeed;
+        playerVelocity = new Vector3(xSpeed, ySpeed, 0.0f);
+
+
+
+        transform.position += playerVelocity * moveSpeed;
     }
 }
--- RileySmithLudwigJam/Assets/Scripts/Rope.cs
+++ RileySmithLudwigJam/Assets/Scripts/Rope.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 //Riley Smith 2021
 //I refrenced this tutorial when writing this code
 //https://www.youtube.com/watch?v=tPtKNvifpj0
-//I highly recomend you watch it rather than just copying the code
-//It will help you understand what you are writing!
+//If you are looking for a base grappling hook you should check out the tutorial
+//as this is a heavily modified version suited to this project specifically.
 //The function of this code is to create a grappling hook that the player can fire to
 //navigate the environment. I still have some adjustments to make so that it is more adaptible.
 
@@ -20,11 +20,14 @@ public class Rope : MonoBehaviour
     [SerializeField] private float waveSize, animSpeed, angle;
 
     private LineRenderer line;
+    private Coroutine grapple;
+    private float length;
+    private bool endOfRope;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        endOfRope = false;
         line = GetComponentInChildren<LineRenderer>();
 
     }
@@ -34,8 +37,14 @@ public class Rope : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            StartCoroutine(routine: AnimateRope(target.position));
+            grapple = StartCoroutine(routine: AnimateRope(target.position));
         }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            StopCoroutine(grapple);
+        }
+
     }
 
     //IEnumerator runs in parallel with the update function
@@ -63,9 +72,23 @@ public class Rope : MonoBehaviour
             yield return null;
 
             //to-do:
-            //add more code so that the rope stays attatched even when it finishes firing
-            //also move the player and restrict player movement once rope is attacthed
-            //also add some grapple spam prevention
+            //add more code so that the rope stays attatched even when it finishes firing - done
+            //also move the player and restrict player movement once rope is attacthed - being done in PlayerController
+            //also add some grapple spam prevention - not done
+        }
+
+        while (!Input.GetMouseButtonDown(1))
+        {
+
+            //calculates the angle we are shooting in case we move durring the shot
+            angle = LookAtAngle(target: targetPos - transform.position);
+
+            percent += Time.deltaTime * animSpeed;
+            AdjustPoints(targetPos, percent, angle);
+            yield return null;
+
+            yield return null;
+
         }
 
         //this line was in the tutorial, not sure if we need it
@@ -78,7 +101,7 @@ public class Rope : MonoBehaviour
         Vector3 ropeEnd = Vector3.Lerp(a: transform.position, b: targetPos, percent);
 
         //gets the current length of the rope
-        float length = Vector2.Distance(a: transform.position, b: ropeEnd);
+        length = Vector2.Distance(a: transform.position, b: ropeEnd);
 
         //looping through all the points in the line renderer
         for (int currentPoint = 0; currentPoint < resolution; currentPoint++)
@@ -97,6 +120,8 @@ public class Rope : MonoBehaviour
             line.SetPosition(currentPoint, pos);
         }
 
+        endOfRope = true;
+
         //multiplying by a direction rotates the vector
         Vector2 RotatePoint(Vector2 point, Vector2 pivot, float angle1)
         {
@@ -108,10 +133,61 @@ public class Rope : MonoBehaviour
 
     }
 
+    //this is largely redundant code, but the mechanism is very fragile and with limited time I don't
+    //think that its worth to spend the time condensing it down to one method with set points
+    private void AdjustPoints(Vector3 targetPos, float percent, float angle)
+    {
+        //figures out where the end of our rope is by checking the percent of the way we have moved bewteen to the two points
+        Vector3 ropeEnd = Vector3.Lerp(a: transform.position, b: targetPos, percent);
+
+        //gets the current length of the rope
+        length = Vector2.Distance(a: transform.position, b: ropeEnd);
+
+        for (int currentPoint = 0; currentPoint < resolution; currentPoint++)
+        {
+            //gives us the position we are along the rope from 0 - 1
+            float xPos = (float)currentPoint / resolution * length;
+            float yPos = Mathf.Sin(f: 0);
+            float reversePercent = 1 - percent;
+
+            Vector2 pos = RotatePoint(new Vector2(x: xPos + transform.position.x, y: yPos + transform.position.y), pivot: transform.position, angle);
+            line.SetPosition(currentPoint, pos);
+        }
+
+        //multiplying by a direction rotates the vector
+        Vector2 RotatePoint(Vector2 point, Vector2 pivot, float angle1)
+        {
+            Vector2 dir = point - pivot;
+            dir = Quaternion.Euler(x: 0, y: 0, z: angle1) * dir;
+            point = dir + pivot;
+            return point;
+        }
+
+
+    }
+
     //calculates the angle we are shooting the rope at
     private float LookAtAngle(Vector2 target)
     {
         return Mathf.Atan2(target.y, target.x) * Mathf.Rad2Deg;
+    }
+
+    //checks if the the grapple has fully reached the target
+    public bool grappleEnded()
+    {
+        return endOfRope;
+    }
+
+    //returns the length of the current rope
+    public float ropeLength()
+    {
+        return length;
+    }
+
+    //returns the position that the end of the rope reached
+    public Vector3 grappleLocation()
+    {
+        return target.position;
     }
 
 }
